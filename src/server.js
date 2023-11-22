f00d3084ce1556fbf594a639e99a041b26936aa3
dzarurizkyy <dzarurizkybusiness@gmail.com>
2023-11-23 00:17:54 +0700

    Create connection with MySQL database

--- src/server.js
+++ src/server.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const dbPool = require('./config/database.js');
 const app = express();
+
+// Connect to database
+dbPool.getConnection((error) => {
+  if (error) {
+    console.log('Connect to database failed...');
+    console.log(error);
+  } else {
+    console.log('Connect to database success...');
+  }
+});
 
 // Create web server
 app.listen(4000, () => {
   console.log('Server running on port 4000');
 });
-
